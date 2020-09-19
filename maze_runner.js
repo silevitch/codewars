1d51add0b47f502677a5f794453c89dadfa77a7f
silevitch <silevitch@users.noreply.github.com>
2020-09-19 11:58:15 -0400

    Create maze_runner.js

---
+++ maze_runner.js
@@ -0,0 +1,57 @@
+// https://www.codewars.com/kata/58663693b359c4a6560001d6/train/javascript
+
+function mazeRunner(maze, directions) {
+  var size = maze[0].length;
+  
+  var start = [];
+  var end = [];
+
+  main:
+    for ( var i = 0; i < size; i++ ){
+      for ( var j = 0; j < size; j++ ) {   
+        if ( start[0] == undefined && maze[i][j] == '2'){
+          start = [i, j];
+        }
+        
+        if ( end[0] == undefined && maze[i][j] == '3'){
+          end = [i, j];
+        }
+        
+        if ( start[0] != undefined && end[0] != undefined ){
+          break main;
+        }
+    }
+  }
+  
+  var current = start;
+
+  for ( var i = 0; i < directions.length; i++ ){
+    if ( directions[i] == 'N' ){
+      current[0]--;
+    }
+    else if( directions[i] == 'S' ) {
+     current[0]++;
+    }
+    else if ( directions[i] == 'W' ){
+      current[1]--;
+    }
+    else {
+      current[1]++;
+    }
+
+    if ( current[0] == -1 || current[0] == maze.length || current[1] == -1 || current[1] == maze.length ){
+      return 'Dead';
+    }
+
+    var spot = maze[ current[0] ][ current[1] ];
+    
+    if ( spot == 1 ){
+      return 'Dead';
+    }
+    else if ( spot == 3 ){
+      return 'Finish';
+    }
+  }
+  
+  return 'Lost';
+}
